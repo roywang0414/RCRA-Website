d9af69ba1740e3b603bebe06a942420fcfe396f5
realroy <roywangking9@gmail.com>
2026-07-07 18:11:50 -0700

    Create _hash.js

---
+++ api/_hash.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+
+function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto.scryptSync(password, salt, 64).toString('hex');
+  return `${salt}:${hash}`;
+}
+
+function verifyPassword(password, stored) {
+  if (!stored || stored.indexOf(':') === -1) return false;
+  const [salt, hash] = stored.split(':');
+  const check = crypto.scryptSync(password, salt, 64).toString('hex');
+  const a = Buffer.from(hash, 'hex');
+  const b = Buffer.from(check, 'hex');
+  if (a.length !== b.length) return false;
+  return crypto.timingSafeEqual(a, b);
+}
+
+module.exports = { hashPassword, verifyPassword };
